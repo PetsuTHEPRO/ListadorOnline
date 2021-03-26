29b00ad5b81a758f0934d13ea80dd2ff23528884
juniornss <juniornunes1008@gmail.com>
2021-03-26 11:53:39 -0300

    Comentários apagados

--- Login/src/com/infinitycorp/model/identity/Client.java
+++ Login/src/com/infinitycorp/model/identity/Client.java
@@ -1,16 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.infinitycorp.model.identity;
 
 import java.util.Date;
 
-/**
- *
- * @author perge
- */
 public class Client {
     
     private int id;
