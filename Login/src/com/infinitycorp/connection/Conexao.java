8e7a73c16ddce06596ac9038b1ac39842fc0df46
perge <perge@10.0.0.111>
2021-03-27 13:16:19 -0300

    Implementado os comandos de CRUD para o ClientDAO

--- Login/src/com/infinitycorp/connection/Conexao.java
+++ Login/src/com/infinitycorp/connection/Conexao.java
@@ -3,25 +3,30 @@ package com.infinitycorp.connection;
 import com.mysql.jdbc.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import javax.swing.JOptionPane;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Conexao {
     
-    public static Connection conector()throws SQLException {
+    public static Connection conector(){
         
         String url = "jdbc:mysql://localhost:3306/dblistagemonline";
-        String user = "root";
-        String password = "";
+        String usuario = "root";
+        String senha = "";
+        Connection conexao = null;
+        String driver = "com.mysql.jdbc.Driver";
+        
         try {
-            Class.forName("com.mysql.jdbc.Driver");
-            return (Connection) DriverManager.getConnection(url, user, password);
-        }catch (ClassNotFoundException e){
-            throw new SQLException(e.getMessage());
+            Class.forName(driver);
+            conexao = (Connection) DriverManager.getConnection(url, usuario, senha);
+            System.out.println("Construido Com Sucesso");
+            return conexao;
+        } catch (ClassNotFoundException ex) {
+            System.out.println("ERRO ao TENTAR Conectar");
+        } catch (SQLException ex){
+            System.out.println("ERRO ao TENTAR Conectar | SQL");
         }
-        // handle any errors
-        
-        
+        return null;
     }
-    
     
 }
--- Login/src/com/infinitycorp/model/DAO/ClientDAO.java
+++ Login/src/com/infinitycorp/model/DAO/ClientDAO.java
@@ -3,20 +3,70 @@ package com.infinitycorp.model.DAO;
 
 import com.infinitycorp.connection.Conexao;
 import com.infinitycorp.model.identity.Client;
+import com.mysql.jdbc.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class ClientDAO {
     
-    private final Conexao connection;
-
+    private final Connection connection;
+  
     public ClientDAO(Conexao connection) {
-        this.connection = connection;
+        this.connection = connection.conector();
     }
     
-    public void insert(Client client){
-        String sql = "inset into client(name, user, password, birthDate) values ()";
-        //PreparedStatement pStatement = connecti
+    public boolean selectClient(Client client) throws SQLException{ 
+            
+            String sql = "select * from tbusuario where nome= ? and senha= ?";
+            
+            PreparedStatement pStatement = connection.prepareStatement(sql);
+            pStatement.setString(1, client.getUser());
+            pStatement.setString(2, client.getPassword());
+            pStatement.execute();
+            
+            ResultSet rSet = pStatement.getResultSet();
+            
+            return rSet.next();
+            
     }
     
+    public void insertClient(Client client) throws SQLException{
+        
+        String sql = "inset into client(name, user, password, birthDate) values (?,?,?,?)";
+        
+        PreparedStatement pStatement = connection.prepareStatement(sql);
+        pStatement.setString(1, client.getName());
+        pStatement.setString(2, client.getUser());
+        pStatement.setString(3, client.getPassword());
+        pStatement.setDate(4, (Date) client.getBirthDate());
+        pStatement.execute();
+        
+    }
+    
+    public void updateClient(Client client) throws SQLException{
+        
+        String sql = "update client set name = ?, user = ?, password = ?, birthDate = ? where id = ?";
+        
+        PreparedStatement pStatement = connection.prepareStatement(sql);
+        pStatement.setString(1, client.getName());
+        pStatement.setString(2, client.getUser());
+        pStatement.setString(3, client.getPassword());
+        pStatement.setDate(4, (Date) client.getBirthDate());
+        //pStatement.setInt(5, client.id);
+        pStatement.execute();
+        
+    }
+    
+    public void deleteClient(Client client) throws SQLException{
+        
+        String sql = "delete from client where id = ?";
+        
+        PreparedStatement pStatement = connection.prepareStatement(sql);
+        //pStatement.setInt(5, client.id);
+        pStatement.execute();
+        
+    }
     
 }
