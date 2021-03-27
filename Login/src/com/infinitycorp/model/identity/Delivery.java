c95aa3c4392c07731614472fc195f5f76f05bbb3
perge <perge@10.0.0.111>
2021-03-27 10:50:30 -0300

    Removi os comentários!

--- Login/src/com/infinitycorp/model/identity/Delivery.java
+++ Login/src/com/infinitycorp/model/identity/Delivery.java
@@ -1,16 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.infinitycorp.model.identity;
 
 import java.util.Date;
 
-/**
- *
- * @author junior nuness
- */
 public class Delivery extends Sweepstakes{
     
     protected Date dateDelivery;
